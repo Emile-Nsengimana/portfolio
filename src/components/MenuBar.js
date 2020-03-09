9423da34fed12214862ac62d96a9c2bef167705d
Emile-Nsengimana <emilereas7@gmail.com>
2020-03-09 17:40:23 +0200

    ft(layout): page layout
    - add webpage sections(about, skill, ...)

---
+++ src/components/MenuBar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+const MenuBar = () => (
+  <nav className="navbar">
+    <span>
+    <Link className="logo"
+        to="home"
+        smooth
+        offset={-200}
+        duration={1000}
+      >
+      Emile Nnsegimana
+      </Link>
+      <span className="user user-status">
+        <FontAwesomeIcon style={{marginRight: "1rem", color: "green"}} icon={faCircle}/>
+        online
+      </span>
+      <span><a className="user user-chat" href="#chat-box">chat now</a></span>
+    </span>
+    <div className="navbar__menu">
+      <Link
+        to="home"
+        smooth
+        offset={-200}
+        duration={1000}
+      >
+        HOME
+      </Link>
+      <Link
+        to="about"
+        smooth
+        offset={-200}
+        duration={2000}
+      >
+      ABOUT
+      </Link>
+      <Link
+        to="project"
+        smooth
+        offset={-100}
+        duration={2000}
+      >
+      PROJECT
+      </Link>
+      <Link
+        to="home"
+        smooth
+        offset={-100}
+        duration={2000}
+      >
+      RESUME
+      </Link>
+    </div>
+  </nav>
+);
+
+export default MenuBar;
